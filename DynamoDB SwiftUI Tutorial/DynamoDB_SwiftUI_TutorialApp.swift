0198da66e79f70db466ccbdd084463822bd19cfb
Francesco Dal Savio <dalsaviof@gmail.com>
2020-10-24 11:45:03 +0200

    Initial Commit

---
+++ DynamoDB SwiftUI Tutorial/DynamoDB_SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamoDB_SwiftUI_TutorialApp.swift
+//  DynamoDB SwiftUI Tutorial
+//
+//  Created by Francesco Dal Savio on 24/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct DynamoDB_SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
